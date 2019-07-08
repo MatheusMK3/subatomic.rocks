0c54df83a1b6a9c7a2f47dbea0424e942a68263c
Matheus Pratta <matpratta@gmail.com>
2019-07-08 01:04:14 -0300

    Contact Test

--- api/contact.js
+++ api/contact.js
@@ -3,20 +3,26 @@ const sendgridMail = require('@sendgrid/mail')
 sendgridMail.setApiKey(process.env.sendgrid_key)
 
 module.exports = async (req, res) => {
-  let { name, email, phone, message } = req.body
+  try {
+    let { name, email, phone, message } = req.body
 
-  let emailMessage = {
-    to: 'oi@subatomic.rocks',
-    from: 'oi@subatomic.rocks',
-    subject: `Contact: ${name}`,
-    text: `From: ${name}, ${email}
-    Phone: ${phone}
-    Message:
-    ${message}`
+    let emailMessage = {
+      to: 'oi@subatomic.rocks',
+      from: 'oi@subatomic.rocks',
+      subject: `Contact: ${name}`,
+      text: `From: ${name}, ${email}
+      Phone: ${phone}
+      Message:
+      ${message}`
+    }
+
+    let response = await sendgridMail.send(emailMessage)
+
+    res.send({ sent: true })
+  } catch (e) {
+    res.status(500).send({
+      sent: false,
+      error: e
+    })
   }
-
-  let response = await sendgridMail.send(emailMessage)
-
-  console.log('Contact', response)
-  res.send('Sent.')
 }
