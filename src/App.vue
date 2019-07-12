f1f2143604d4204f4e3cc9d0a560259d0f4ce3e6
Matheus Pratta <matpratta@gmail.com>
2019-07-11 23:43:59 -0300

    Pre-release preview

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <navigation :color="$color" />
     <website-main />
+
+    <link rel="icon" type="image/png" :href="appIcon" />
   </div>
 </template>
 
@@ -12,7 +14,8 @@ import Viewport from './viewport'
 export default {
   data () {
     return {
-      darkColorScheme: false
+      darkColorScheme: false,
+      appIcon: require('./assets/logo.png')
     }
   },
   computed: {
@@ -22,6 +25,23 @@ export default {
   },
   created () {
     events.$on('dark-color-scheme', enabled => { this.darkColorScheme = enabled })
+
+    // Google Tag Manager / Analytics
+    const GA_TRACKING_CODE = 'UA-127809460-1'
+    let gtm = document.createElement('script')
+      gtm.setAttribute('async', true)
+      gtm.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-127809460-1')
+    document.head.appendChild(gtm)
+
+    window.dataLayer = window.dataLayer || []
+    function gtag () { window.dataLayer.push(arguments) }
+    gtag('js', new Date())
+    gtag('config', GA_TRACKING_CODE)
+
+    // GTM after page change
+    this.$router.afterEach((to, from) => {
+      gtag('config', GA_TRACKING_CODE, { 'page_path': this.$router.currentRoute.path })
+    })
     
     // Hooks window resizes to app update
     Viewport.hook(e => { this.$forceUpdate() })
